cdc561f292245393f2b6265a11d40294fc3251fe
keieng <105902831+keieng@users.noreply.github.com>
2022-07-06 23:38:20 +0900

    追加、削除機能をカスタムフック化

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,19 @@
 import { ChangeEvent, useState, FC, useCallback } from "react";
 import styled from "styled-components";
 import { MemoList } from "./MemoList";
-
+import { useMemoList } from "../hooks/useMemoList";
 export const App: FC = () => {
+  // カスタムフックからそれぞれ取得
+  const { memos, addTodo, deleteTodo } = useMemoList();
   // テキストボックスState
   const [text, setText] = useState<string>("");
-  // メモ一覧State
-  const [memos, setMemos] = useState<string[]>([]);
-
   // テキストボックス入力時に入力内容をStateに設定
   const onChangeText = (e: ChangeEvent<HTMLInputElement>) =>
     setText(e.target.value);
 
   // [追加]ボタン押下時
   const onClickAdd = () => {
-    // State変更を正常に検知させる為新たな配列を生成
-    const newMemos = [...memos];
-    // テキストボックスの入力内容をメモ配列に追加
-    newMemos.push(text);
-    setMemos(newMemos);
+    addTodo(text);
     // テキストボックスを空に
     setText("");
   };
@@ -26,13 +21,9 @@ export const App: FC = () => {
   // [削除]ボタン押下時（何番目が押されたかを引数で受け取る）
   const onClickDelete = useCallback(
     (index: number) => {
-      // State変更を正常に検知させるため新たな配列を生成
-      const newMemos = [...memos];
-      // メモ配列から要素を削除
-      newMemos.splice(index, 1);
-      setMemos(newMemos);
+      deleteTodo(index);
     },
-    [memos]
+    [deleteTodo]
   );
 
   return (
---
+++ src/hooks/useMemoList.ts
@@ -0,0 +1,31 @@
+import { useCallback, useState } from "react";
+
+// メモ一覧に関するカスタムフック
+export const useMemoList = () => {
+  // メモ一覧State
+  const [memos, setMemos] = useState<string[]>([]);
+  //メモ追加ロジック
+  const addTodo = useCallback(
+    (text: string) => {
+      // State変更を正常に検知させる為新たな配列を生成
+      const newMemos = [...memos];
+      // テキストボックスの入力内容をメモ配列に追加
+      newMemos.push(text);
+      setMemos(newMemos);
+      // 依存配列にmemosを設定
+    },
+    [memos]
+  );
+  // メモ削除ロジック
+  const deleteTodo = useCallback(
+    (index: number) => {
+      // State変更を正常に検知させるため新たな配列を生成
+      const newMemos = [...memos];
+      // メモ配列から要素を削除
+      newMemos.splice(index, 1);
+      setMemos(newMemos);
+    },
+    [memos]
+  );
+  return { memos, addTodo, deleteTodo };
+};
